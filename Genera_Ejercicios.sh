ff17f56c0ee8b12a51ef6563212b05e9117088ce
upszot <upszot@gmail.com>
2024-10-11 06:08:00 -0300

    armando

--- Genera_Ejercicios.sh
+++ Genera_Ejercicios.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [ -d $HOME/Defensa/ ]; then
+    echo "Por favor Ejecute: "
+    echo "cd $HOME/Defensa/ "
+    echo "cat $HOME/Defensa/Ejercicios.txt"
+    exit 1
+fi
 
+mkdir -p $HOME/Defensa/
+PARCIAL="UTNFRA_SO_1P2C_2024"
 DIR_BASE=$(dirname "$(readlink -f "$0")")
 SCRIPT_DIR="${DIR_BASE}/.Enunciados"
 
@@ -13,26 +21,14 @@ NroPermisos="$(( ( RANDOM % 2 ) + 1 ))"
 #echo " Se generaran los ejerciciso NroUser: $NroUser"
 #echo " Se generaran los ejerciciso NroPermisos: $NroPermisos"
 
-
-if [ -d $HOME/Defensa/ ]; then
-    echo "Por favor Ejecute: "
-    echo "cd $HOME/Defensa/ "
-    echo "cat $HOME/Defensa/Ejercicios.txt"
-    exit 1
-fi
-
-mkdir -p $HOME/Defensa/
-
-PARCIAL="UTNFRA_SO_1P2C_2024"
-
-
 # --- Genera Enunciado ---#
-#Estructura:
+# Estructura:
 RTA=$(gpg --batch --yes --passphrase "$PARCIAL" --output - -d ${SCRIPT_DIR}/E_Estructura_${NroEstructura}.txt.e > $HOME/Defensa/Ejercicios.txt 2> /dev/null)
-#Permisos:
+# Permisos:
 mkdir $HOME/Defensa/Permisos
 RTA=$(gpg --batch --yes --passphrase "$PARCIAL" --output - -d $SCRIPT_DIR/E_Permisos_$NroPermisos.txt.e   >> $HOME/Defensa/Ejercicios.txt 2> /dev/null)
 RTA=$(gpg --batch --yes --passphrase "$PARCIAL" --output - -d $SCRIPT_DIR/E_Permisos_$NroPermisos.txt.e   > $HOME/Defensa/Permisos/archivo.txt 2> /dev/null)
+
 sudo chattr +i $HOME/Defensa/Ejercicios.txt
 sudo chattr +i $HOME/Defensa/Permisos/archivo.txt
 
