34c21bb746e0b6261d6ddd63e7161a4414635314
upszot <upszot@gmail.com>
2024-10-14 06:15:14 -0300

    ADD: Ejercicios

--- _checks_Defensa/check_D.sh
+++ _checks_Defensa/check_D.sh
@@ -9,26 +9,16 @@ MAPEO_FILE="$DIR_BASE/.soporte/Punto_${E_NRO}_Mapeo.e"
 
 gpg --batch --yes --passphrase "$PARCIAL" --output ${MAPEO_FILE}.txt -d $MAPEO_FILE > /dev/null 2>&1
 
-
+whoami_UG=$(echo "$(whoami),$(id -ng $(whoami))")
+owner_Path=$(ls -ld /home/vagrant/Defensa/Permisos/ | awk '{print $3","$4}')
 sudo find /home/vagrant/Defensa/Permisos/ -exec sudo stat -c %a" "%n {} \; | sed "s|$HOME/Defensa/||g" |sort -k2 > "$PATH_TEMP/Punto_D_Mapeo_Alumno.txt"
 
-sudo stat -c %a,%U,%G /home/vagrant/Defensa/Permisos/   > "$PATH_TEMP/Punto_D_Mapeo_Alumno.txt"
-sudo stat -c %a,%U,%G /home/vagrant/Defensa/Permisos/archivo.txt > "$PATH_TEMP/Punto_D_Mapeo_Alumno.txt"
-UGP=$(sudo stat -c %U,%G /home/vagrant/Defensa/Permisos/)
-
-
 # Verifico que no haya cambiado los owner
-whoami_UG="$($USER,$(id -ng $(whoami)))"
-owner_Path=$(ls -ld /home/vagrant/Defensa/Permisos/ | awk '{print $3","$4}')
-
 if [ "$owner_Path" != "$whoami_UG" ]; then
     echo "Punto D - Error Code: 1 - Mensaje: Owner Modificado"
 fi
 
-
-echo
 echo "Validar Segun Enunciado: ($E_NRO)"
-echo
 # Comparar el resultado con el archivo de soporte
 if diff "$PATH_TEMP/Punto_D_Mapeo_Alumno.txt" "${MAPEO_FILE}.txt" > /dev/null; then
     echo "Punto D - OK"
@@ -37,13 +27,15 @@ else
 fi
 
 echo
-echo "Permisos directorio:"
+echo "ls -ld  $HOME/Defensa/Permisos "
 ls -ld  $HOME/Defensa/Permisos
 echo
-echo "Permisos Dentro del directorio:"
+echo "ls -l  $HOME/Defensa/Permisos "
 ls -l  $HOME/Defensa/Permisos
 echo
-echo "Contenido del archivo: "
+echo "cat  $HOME/Defensa/Permisos/archivo.txt "
 cat  $HOME/Defensa/Permisos/archivo.txt
 echo
-echo
+
+rm -rf ${MAPEO_FILE}.txt
+
